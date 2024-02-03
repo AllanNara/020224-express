772cc492b5a4df55b6fb2dbdadb39ae57a3f3839
AllanNara <allannara@outlook.com>
2024-02-02 21:27:23 -0300

    mongoose conection

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import config from "./config/config.js";
 import app from "./src/app.js";
-import connectMongo from "./src/mongoose.js"
+import connectMongo from "./src/mongoose.js";
 
-startServer();
-
-async function startServer() {
-  const db = await connectMongo()
-
-  app.listen(config.PORT, () => {
-    console.log(`Server listening on port ${config.PORT}`)
-    db.on("once", () => console.log("Up database"))
-    db.on("error", (e) => console.log("Error in mongodb: ", e))
-  })
-}
-
+const db = connectMongo();
+db.on("connected", () => {
+	console.log(`MongoDB connected successfully!`);
+	app.listen(config.PORT, () => {
+		console.log(`Server listening on port ${config.PORT}`);
+	});
+});
--- src/mongoose.js
+++ src/mongoose.js
@@ -1,16 +1,37 @@
 import mongoose from "mongoose";
 import config from "../config/config.js";
 
-async function connectMongo() {
-  try {
-    await mongoose.connect(config.MONGO_URI);
-    console.log(`MongoDB connected successfully!`);
-  } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error}`);
-    throw error
-  }
+function connectMongo() {
 
-  return mongoose.connection;
+  mongoose.connect(config.MONGO_URI);
+
+  const db = mongoose.connection;
+
+  db.on("connecting", () => {
+    console.log("Conectándose a la base de datos MongoDB...");
+  });
+
+  db.on("disconnecting", () => {
+    console.log("Desconectándose de la base de datos MongoDB...");
+  });
+
+  db.on("disconnected", () => {
+    console.log("Desconexión completa de la base de datos MongoDB");
+  });
+
+  db.on("error", (err) => {
+    console.error(`Error connecting to MongoDB: ${err}`);
+  });
+
+  db.on("reconnected", () => {
+    console.log("Reconexión exitosa a la base de datos MongoDB");
+  });
+
+  db.on("reconnectFailed", () => {
+    console.error("Error al intentar reconectar a la base de datos MongoDB");
+  });
+
+  return db;
 } 
 
 export default connectMongo
